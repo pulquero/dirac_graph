8ea77a631158f6ec961ef1073762119e79a247cc
Mark Hale <mark.hale@physics.org>
2021-01-22 20:03:42 +0000

    Published as a package.

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 A Python library that makes it easy to perform computational spectral geometry on finite graphs.
 
+## Installation
+
+Dirac on a Graph is available from PyPI:
+
+	pip install dirac_graph
+
+## Examples
+
 [Example notebook](https://github.com/pulquero/dirac_graph/blob/main/example.ipynb) demonstrating various calculations.
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dirac_graph",
+    version="1.0.0",
+    author="Mark Hale",
+    license="MIT",
+    description="Computational spectral geometry on finite graphs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pulquero/dirac_graph",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
